6c4f5f873ae5787dd103c368785a544bbd096dd4
jvmoreirad <jvitormoreirad@hotmail.com>
2023-01-16 17:25:29 -0300

    fix: change to colorGrid docs

--- packages/docs/src/Components/ColorsGrid.tsx
+++ packages/docs/src/Components/ColorsGrid.tsx
@@ -10,7 +10,7 @@ export function ColorsGrid() {
             display: 'flex',
             justifyContent: 'space-between',
             fontFamily: 'monospace',
-            color: getContrast(color, '#FFFF') < 3.5 ? '#000' : '#FFFF',
+            color: getContrast(color, '#FFF') < 3.5 ? '#000' : '#FFF',
           }}
         >
           <strong>${key}</strong>
